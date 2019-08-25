7e63c9093abdfd761018b473fc6d2b79bc3a4add
Tanner Holladay <TannerHolladay6@gmail.com>
2019-08-25 15:28:55 -0600

    Added Gun

--- cgj-fall-2019/Assets/_models/Player/CaracterController.cs
+++ cgj-fall-2019/Assets/_models/Player/CaracterController.cs
@@ -1,55 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Player.Guns;
 using UnityEngine;
 
-public class CaracterController : MonoBehaviour
+namespace Player
 {
-    private Rigidbody2D _rigidbody;
-    private SpriteRenderer _sprite;
-    private float _horizontal;
-    private float _vertical;
-    public float walkSpeed = 5;
-    public bool lockMovment = false;
-
-    // Start is called before the first frame update
-    void Start()
+    public class CaracterController : MonoBehaviour
     {
-        _rigidbody = GetComponent<Rigidbody2D>();
-        _sprite = GetComponentInChildren<SpriteRenderer>();
-    }
+        private Rigidbody2D _rigidbody;
+        private SpriteRenderer _sprite;
+        private float _horizontal;
+        private float _vertical;
 
-    public void LockMovment()
-    {
-        lockMovment = true;
-    }
+        public float walkSpeed = 5;
+        public Gun currentGun;
 
-    public void UnlockMovment()
-    {
-        lockMovment = false;
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+            _sprite = GetComponentInChildren<SpriteRenderer>();
+        }
 
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (!lockMovment)
+        // Update is called once per frame
+        void Update()
         {
             _horizontal = Input.GetAxisRaw("Horizontal");
             _vertical = Input.GetAxisRaw("Vertical");
             _rigidbody.velocity = new Vector2(_horizontal, _vertical).normalized * walkSpeed;
             var velocity = _rigidbody.velocity;
-       
             if (velocity.magnitude > 0)
             {
                 var angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
                 _sprite.transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
             }
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                currentGun.FirePressed(Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position));
+            }
         }
-        else
+
+        public void EquipWeapon(Gun gun)
         {
-            _horizontal = 0;
-            _vertical = 0;
-            _rigidbody.velocity = new Vector2(0,0);
-        }   
+            
+        }
     }
 }
---
+++ cgj-fall-2019/Assets/_models/Player/Guns/Scripts/Pistol.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Player.Guns
+{
+    public class Pistol : Gun
+    {
+        public override void FirePressed(Vector2 direction)
+        {
+            var bullet = NewBullet();
+            StartCoroutine(Shoot(bullet, direction));
+            Destroy(bullet, bulletLife);
+        }
+    }
+}
