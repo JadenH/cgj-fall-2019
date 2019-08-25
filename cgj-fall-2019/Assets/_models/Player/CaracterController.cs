b97184a35ed72f147ef5c7c8b9595c80e7da3646
austinGENEreeve <austinGENEreeve@gmail.com>
2019-08-25 14:44:43 -0700

    bullet player stuff

--- cgj-fall-2019/Assets/_models/Player/CaracterController.cs
+++ cgj-fall-2019/Assets/_models/Player/CaracterController.cs
@@ -10,6 +10,7 @@ namespace Player
         private float _horizontal;
         private float _vertical;
 
+        public bool LockMovement = false;
         public float walkSpeed = 5;
         public Gun currentGun;
 
@@ -19,24 +20,42 @@ namespace Player
             _rigidbody = GetComponent<Rigidbody2D>();
             _sprite = GetComponentInChildren<SpriteRenderer>();
         }
+        public void LockMovment()
+        {
+            LockMovement = true;
+        }
 
+        public void UnlockMovment()
+        {
+            LockMovement = false;
+        }
         // Update is called once per frame
         void Update()
         {
-            _horizontal = Input.GetAxisRaw("Horizontal");
-            _vertical = Input.GetAxisRaw("Vertical");
-            _rigidbody.velocity = new Vector2(_horizontal, _vertical).normalized * walkSpeed;
-            var velocity = _rigidbody.velocity;
-            if (velocity.magnitude > 0)
+            if(!LockMovement)
             {
-                var angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
-                _sprite.transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
-            }
+                _horizontal = Input.GetAxisRaw("Horizontal");
+                _vertical = Input.GetAxisRaw("Vertical");
+                _rigidbody.velocity = new Vector2(_horizontal, _vertical).normalized * walkSpeed;
+                var velocity = _rigidbody.velocity;
+                if (velocity.magnitude > 0)
+                {
+                    var angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
+                    _sprite.transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+                }
 
-            if (Input.GetButtonDown("Fire1"))
-            {
-                currentGun.FirePressed(Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position));
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    currentGun.FirePressed(Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position));
+                }
             }
+            else
+            {
+                _horizontal = 0;
+                _vertical = 0;
+                _rigidbody.velocity = new Vector2(0, 0);
+            }
+           
         }
 
         public void EquipWeapon(Gun gun)
