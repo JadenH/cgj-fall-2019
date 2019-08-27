58f64c23da6c58b3bdc58daa13d7352483f87ff8
austinGENEreeve <austinGENEreeve@gmail.com>
2019-08-26 20:16:31 -0700

    MiniMap

---
+++ cgj-fall-2019/Assets/_scripts/Environment/MiniMap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour
+{
+    public Camera cam1;
+    public Camera cam2;
+
+    void Start()
+    {
+        cam1.enabled = true;
+        cam2.enabled = false;
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            cam1.enabled = false;
+            cam2.enabled = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            cam1.enabled = true;
+            cam2.enabled = false;
+        }
+    }
+}
