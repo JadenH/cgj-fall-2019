7d72f3ecfd432ec1a0133000534db651c9886e1e
austinGENEreeve <austinGENEreeve@gmail.com>
2019-08-26 19:23:20 -0700

    Revert "merge"
    This reverts commit 4c304c17b12032a471e880e91611f2149d15a6e3, reversing
    changes made to 2d1ee0eb4ff7fa9e96fc212363c2f0c7e247f55e.

--- cgj-fall-2019/Assets/_scripts/Player/CharacterController.cs
+++ cgj-fall-2019/Assets/_scripts/Player/CharacterController.cs
@@ -1,55 +1,51 @@
 ﻿using UnityEngine;
 
-public class Character : MonoBehaviour
+public class CharacterController : MonoBehaviour
 {
     private Rigidbody2D _rigidbody;
+    private SpriteRenderer _sprite;
     private float _horizontal;
     private float _vertical;
-    private Gun _nearbyGun;
 
     public bool LockMovement;
     public float WalkSpeed = 5;
     public Gun CurrentGun;
-    public GameObject PickupInterface;
 
+    // Start is called before the first frame update
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
-        if (CurrentGun)
-        {
-            CurrentGun.GetComponent<CircleCollider2D>().enabled = false;
-        }
+        _sprite = GetComponentInChildren<SpriteRenderer>();
     }
 
+    // Update is called once per frame
     private void Update()
     {
-        if (_nearbyGun && Input.GetKeyDown(KeyCode.E))
-        {
-            EquipWeapon(_nearbyGun);
-        }
         if (!LockMovement)
         {
             _horizontal = Input.GetAxisRaw("Horizontal");
             _vertical = Input.GetAxisRaw("Vertical");
             _rigidbody.velocity = new Vector2(_horizontal, _vertical).normalized * WalkSpeed;
+            var velocity = _rigidbody.velocity;
+            if (velocity.magnitude > 0)
+            {
+                //var angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
+                //_sprite.transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+            }
 
             var lookDir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
             var angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
 
             var gunDir = Input.mousePosition - Camera.main.WorldToScreenPoint(CurrentGun.transform.position);
-            if (CurrentGun.CurrentCooldown <= Time.time)
+            if (Input.GetButtonDown("Fire"))
             {
-                if (Input.GetButtonDown("Fire"))
-                {
-                    CurrentGun.FirePressed(gunDir);
-                }
+                CurrentGun.FirePressed(gunDir);
+            }
 
-                if (Input.GetButton("Fire"))
-                {
-                    CurrentGun.FireHold(gunDir);
-                }
-                CurrentGun.CurrentCooldown = Time.time + CurrentGun.Cooldown;
+            if (Input.GetButton("Fire"))
+            {
+                CurrentGun.FireHold(gunDir);
             }
         }
         else
@@ -64,30 +60,11 @@ public class Character : MonoBehaviour
     public void EquipWeapon(Gun gun)
     {
         var gunTransform = gun.transform;
-        CurrentGun.transform.position = gunTransform.position;
+        gunTransform.position = CurrentGun.transform.position;
         CurrentGun.transform.parent = gunTransform.parent;
         CurrentGun.GetComponent<Collider2D>().enabled = true;
         gun.transform.parent = transform;
         gun.GetComponent<Collider2D>().enabled = false;
-        gun.transform.localPosition = new Vector3(-0.3f, 0.2f, 0);
         CurrentGun = gun;
-    }
-
-    public void OnTriggerEnter2D(Collider2D other)
-    {
-        _nearbyGun = other.GetComponent<Gun>();
-        if (_nearbyGun)
-        {
-            PickupInterface.SetActive(true);
-        }
-    }
-
-    public void OnTriggerExit2D(Collider2D other)
-    {
-        if (_nearbyGun && other.transform == _nearbyGun.transform)
-        {
-            PickupInterface.SetActive(false);
-            _nearbyGun = null;
-        }
     }
 }
