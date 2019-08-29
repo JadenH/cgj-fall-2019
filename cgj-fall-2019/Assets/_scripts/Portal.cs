cfc5a42af18b82c873765528b2f9091326e7724e
austinGENEreeve <austinGENEreeve@gmail.com>
2019-08-28 18:19:25 -0700

    Portal Text

---
+++ cgj-fall-2019/Assets/_scripts/Portal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+
+    public GameObject Canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Canvas.SetActive(true);
+        print("SADfas");
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Canvas.SetActive(false);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
