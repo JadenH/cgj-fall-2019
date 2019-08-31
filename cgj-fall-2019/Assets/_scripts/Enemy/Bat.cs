724e4bdca0c08aa33cc40c615f31499546fcec3b
jadenh <jadenholladay@gmail.com>
2019-08-30 21:58:20 -0600

    update bat art

--- cgj-fall-2019/Assets/_scripts/Enemy/Bat.cs
+++ cgj-fall-2019/Assets/_scripts/Enemy/Bat.cs
@@ -3,15 +3,18 @@ using System.Collections;
 
 [RequireComponent(typeof(Chaser))]
 [RequireComponent(typeof(Mover))]
+[RequireComponent(typeof(Health))]
 public class Bat : Enemy
 {
     private Chaser _chaser;
     private Mover _mover;
+    private Health _health;
 
     private void Awake()
     {
         _chaser = GetComponent<Chaser>();
         _mover = GetComponent<Mover>();
+        _health = GetComponent<Health>();
     }
 
     protected override void PlayerChangedRooms(Room room)
